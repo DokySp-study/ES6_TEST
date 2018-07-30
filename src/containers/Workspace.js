e9d6d2f18adf98a4b2447dc4e19fc2226f2c50c6
김도균 <uhug@gimdogyun-ui-MacBook-Pro.local>
2018-07-30 15:50:55 +0900

    Ready to make console

--- src/containers/Workspace.js
+++ src/containers/Workspace.js
@@ -17,3 +17,23 @@ class Workspace extends React.Component {
 }
 
 export default Workspace;
+
+//
+// 구글 맵 관련 API
+//  - https://www.npmjs.com/package/react-google-map (508)
+//  - https://www.npmjs.com/package/google-maps-react (7,793)
+//
+// Google 본문 문서
+//  - https://developers.google.com/maps/documentation/javascript/adding-a-google-map
+//
+// 애니메이션
+//  - https://reactjs.org/docs/animation.html
+//
+
+
+
+
+// Editor.js
+// Directory.js 
+// FileDirectory.js
+// Map.js
